161fed5cd2433253b5303d8e9c960325011c037f
Victoria Shvareva <victoria.shvareva@gmx.ch>
2016-02-28 22:41:09 +0100

    Задание №5

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupHelper extends HelperBase {
+
+  public GroupHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void returnToGroupPage() {
+    click(By.linkText("group page"));
+  }
+
+  public void submitGroupCreation() {
+      click(By.name("submit"));
+  }
+
+  public void filGroupForm(GroupData groupData) {
+    type(By.name("group_name"), groupData.getName());
+    type(By.name("group_header"), groupData.getHeader());
+    type(By.name("group_footer"), groupData.getFooter());
+  }
+
+  public void initGroupCreation() {
+    click(By.name("new"));
+  }
+
+  public void deleteSelectedGroups() {
+    click(By.xpath("//div[@id='content']/form/input[5]"));
+  }
+
+  public void selectGroup() {
+    click(By.name("selected[]"));
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+    @Test
+    public void testGroupCreation() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().filGroupForm(new GroupData("Test1", "Test2", "Test3"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+}
