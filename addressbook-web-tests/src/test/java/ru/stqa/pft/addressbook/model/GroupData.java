fa85842ca1612a18d3010844e7a6dda1a1814cf0
Victoria Shvareva <victoria.shvareva@gmx.ch>
2016-04-02 19:53:40 +0200

    Урок 6 видео 6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,9 +1,15 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
+@XStreamAlias("group")
 public class GroupData {
+
   private String name;
   private String header;
   private String footer;
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
 
   // getters
